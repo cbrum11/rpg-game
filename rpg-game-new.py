bea7214c50e362fb8bfafbc737d67a9904839501
Chase Brumfield <brumfieldchase@gmail.com>
2019-04-24 13:08:02 -0500

    completed zombie character

--- rpg-game-new.py
+++ rpg-game-new.py
@@ -22,16 +22,17 @@ class Character():
 # Initialize Characters
 goblin = Character('goblin', 6, 2)
 hero = Character('hero', 10, 5)
-zombie = Character('zombie', )
+zombie = Character('zombie', 1000000, 1000000)
 
 while goblin.alive() and hero.alive():
     print("You have {} health and {} power.".format(hero.health, hero.power))
     print("The goblin has {} health and {} power.".format(goblin.health, goblin.power))
-    print()
+    print("The zombie has {} health and {} power.".format(zombie.health, zombie.power))
     print("What do you want to do?")
     print("1. fight goblin")
     print("2. do nothing")
     print("3. flee")
+    print("4. fight zombie")
     print("> ", end=' ')
     raw_input = input()
     if raw_input == "1":
@@ -41,8 +42,15 @@ while goblin.alive() and hero.alive():
     elif raw_input == "3":
         print("Goodbye.")
         break
+    elif raw_input == "4":
+        hero.attack(zombie)
+        zombie_fg = True # Skip goblin once
     else:
         print("Invalid input {}".format(raw_input))
 
-    if goblin.health > 0:
+    if goblin.health > 0 and zombie_fg == False:
         goblin.attack(hero)
+    else:
+        zombie.attack(hero)
+        zombie_fg == False
+    
