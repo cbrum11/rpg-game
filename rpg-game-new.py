6fee98e1a5fb823f64299d08fa650542d6d399e3
Chase Brumfield <brumfieldchase@gmail.com>
2019-04-24 12:06:30 -0500

    initialize Hero & Goblin class

---
+++ rpg-game-new.py
@@ -0,0 +1,20 @@
+class Hero():
+    def __init__(self, health, pohwer):
+    self.health = health
+    self.power = power
+
+class Goblin():
+    def __init__(self, health, pohwer):
+    self.health = health
+    self.power = power
+
+hero_health = 10
+hero_power = 5
+goblin_health = 6
+goblin_power = 2
+
+G1 = Goblin(6,2)
+H1 = Hero(10,5)
+
+
+while Go
